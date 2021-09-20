c09167ed1a7ee9ff42f5be906055fd8127bde22d
Charles Nurse <charlesnurse@gmail.com>
2021-09-19 17:28:05 -0700

    Add AdminController and BlogSettings View Up version to 2.3.0-beta.3

---
+++ src/Naif.Blog.UI/Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Naif.Blog.Framework;
+using Naif.Blog.Services;
+using Naif.Blog.UI.ViewModels;
+
+namespace Naif.Blog.UI.Controllers
+{
+    [Route("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        private readonly IBlogManager _blogManager;
+
+        public AdminController(IBlogContext blogContext, IBlogManager blogManager)
+        {
+            Blog = blogContext.Blog;
+            _blogManager = blogManager;
+        }
+        
+        public Models.Blog Blog { get; }
+
+        [Route("BlogSettings")]
+        public IActionResult Index()
+        {
+            var blogViewModel = new BlogViewModel
+            {
+                Blog = Blog,
+            };
+            
+            // ReSharper disable once Mvc.ViewNotResolved
+            return View("BlogSettings", blogViewModel);           
+
+        }
+    }
+}
