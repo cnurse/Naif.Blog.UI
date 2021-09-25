a9d56d08f21b95ae2cafb855c8059818b39e5817
Charles Nurse <charlesnurse@gmail.com>
2021-09-25 16:16:42 -0700

    remove Errors and change Messages to be a List of Message objects Up version to 2.3.0-beta.7

--- src/Naif.Blog.UI/ViewModels/BlogViewModel.cs
+++ src/Naif.Blog.UI/ViewModels/BlogViewModel.cs
@@ -9,9 +9,7 @@ namespace Naif.Blog.UI.ViewModels
         
         public Naif.Blog.Models.Blog Blog { get; set; }
         
-        public string[] Errors { get; set; }
-        
-        public string[] Messages { get; set; }
+        public IList<Message> Messages { get; set; }
         
         public int PageIndex { get; set; }
         
