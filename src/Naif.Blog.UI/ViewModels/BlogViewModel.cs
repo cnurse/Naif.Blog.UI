dfe325a781e4a4753381f303eec531ac23a087a8
Charles Nurse <charlesnurse@gmail.com>
2021-09-25 16:03:50 -0700

    Add Errors and Messages arrays to BlogViewModel Up version to 2.3.0-beta.6

--- src/Naif.Blog.UI/ViewModels/BlogViewModel.cs
+++ src/Naif.Blog.UI/ViewModels/BlogViewModel.cs
@@ -5,14 +5,18 @@ namespace Naif.Blog.UI.ViewModels
 {
     public class BlogViewModel
     {
+        public string BaseUrl { get; set; }
+        
         public Naif.Blog.Models.Blog Blog { get; set; }
+        
+        public string[] Errors { get; set; }
+        
+        public string[] Messages { get; set; }
         
         public int PageIndex { get; set; }
         
         public Post Post { get; set; }
 
         public IEnumerable<Post> Posts { get; set; }
-        
-        public string BaseUrl { get; set; }
     }
 }
