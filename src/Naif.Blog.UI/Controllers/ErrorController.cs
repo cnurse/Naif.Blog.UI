a4d9a5ef5572e474331d0d07cd2aa721893ab69a
Charles Nurse <charlesnurse@gmail.com>
2021-10-14 11:39:45 -0700

    Update ErrorController Up version to 2.3.1-alpha.11

--- src/Naif.Blog.UI/Controllers/ErrorController.cs
+++ src/Naif.Blog.UI/Controllers/ErrorController.cs
@@ -1,18 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Naif.Blog.Controllers;
 using Naif.Blog.Framework;
+using Naif.Blog.Services;
 using Naif.Blog.UI.ViewModels;
 
 namespace Naif.Blog.UI.Controllers
 {
-    public class ErrorController : BaseController
+    public class ErrorController : BaseUIController
     {
-        public ErrorController(IBlogContext blogContext)
+        public ErrorController(IBlogContext blogContext, IBlogManager blogManager) : base(blogContext, blogManager)
         {
-            Blog = blogContext.Blog;
         }
-
-        public Models.Blog Blog { get; }
 
         public IActionResult Index()
         {
@@ -24,15 +22,15 @@ namespace Naif.Blog.UI.Controllers
         {
             if (errCode == "404")
             {
-                return View("404", new BlogViewModel { Blog = Blog});
+                return View("404", ViewModel);
             }
 
             if (errCode == "500")
             {
-                return View("500", new BlogViewModel { Blog = Blog});
+                return View("500", ViewModel);
             }
 
-            return View("Unknown", new BlogViewModel { Blog = Blog});
+            return View("Unknown", ViewModel);
         }
     }
 }
