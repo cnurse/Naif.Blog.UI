1a53da77830081ac0a9f42ffcf9e6b63902f5785
Charles Nurse <charlesnurse@gmail.com>
2021-11-11 11:31:43 -0800

    Move Home Controller and Views from Web project

---
+++ src/Naif.Blog.UI/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Naif.Blog.Controllers;
+using Naif.Blog.Framework;
+using Naif.Blog.ViewModels;
+using Naif.Core.Models;
+
+namespace Naif.Blog.UI.Controllers
+{
+    [Route("Home")]
+    public class HomeController : BaseController
+    {
+        private User _user;
+        
+        public HomeController(IBlogContext blogContext)
+        {
+            Blog = blogContext.Blog;
+            _user = blogContext.User;
+        }
+
+        public Models.Blog Blog { get; }
+
+        [Route("Index")]
+        [Route("~/")]
+        public IActionResult Index()
+        {
+            IActionResult result;
+
+            if (String.IsNullOrEmpty(Blog.HomeRedirectUrl))
+            {
+                result = View(new BlogViewModel { Blog = Blog });
+            }
+            else
+            {
+                result = Redirect(Blog.HomeRedirectUrl);
+            }
+
+            return result;
+        }
+
+    }
+}
